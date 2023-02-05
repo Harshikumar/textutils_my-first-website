52081b9792938fb30835863ddc77d93bbd80ff02
Harshikumar <kumar20harshith@gmail.com>
2023-02-05 11:07:49 +0530

    Updates

---
+++ precache-manifest.784e61b6e1abe973563db48566950429.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea44a93512ef8af4d46d",
+    "url": "/textutils_my-first-website/static/css/main.f04b8fe7.chunk.css"
+  },
+  {
+    "revision": "ea44a93512ef8af4d46d",
+    "url": "/textutils_my-first-website/static/js/main.ea44a935.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/textutils_my-first-website/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/textutils_my-first-website/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "f3f627210089f41a465b",
+    "url": "/textutils_my-first-website/static/js/runtime~main.f3f62721.js"
+  },
+  {
+    "revision": "9189892b67593d88cb4a3e8de199bbcb",
+    "url": "/textutils_my-first-website/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/textutils_my-first-website/precache-manifest.784e61b6e1abe973563db48566950429.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/textutils_my-first-website/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
